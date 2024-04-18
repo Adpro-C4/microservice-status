caa562379fd63975acd7c97d63e1866dedc5843d
Failasuf Indi Marsendy <failasufindi123@gmail.com>
2024-04-19 02:11:57 +0700

    first

---
+++ src/test/java/id/ac/ui/cs/advprog/statustrackingorder/StatusTrackingOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.statustrackingorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusTrackingOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

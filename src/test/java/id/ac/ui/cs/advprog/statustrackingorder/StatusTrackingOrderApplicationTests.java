d9290171de2c631acbf7c4c0f19ec73358d91b9f
Failasuf Indi Marsendy <failasufindi123@gmail.com>
2024-04-26 04:33:16 +0700

    update test

--- src/test/java/id/ac/ui/cs/advprog/statustrackingorder/StatusTrackingOrderApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/statustrackingorder/StatusTrackingOrderApplicationTests.java
@@ -3,7 +3,7 @@ package id.ac.ui.cs.advprog.statustrackingorder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class StatusTrackingOrderApplicationTests {
 
     @Test
